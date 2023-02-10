9802e89e1df8572dc5a6fc4985458fec61e1ad2d
ocastelblack <oscard.castelblanco@gmail.com>
2023-02-10 04:17:28 -0500

    Modificar cliente

--- pruebaTecnica/Controllers/ClientesController.cs
+++ pruebaTecnica/Controllers/ClientesController.cs
@@ -51,7 +51,7 @@ namespace pruebaTecnica.Controllers
 
             if (longitudApellido.Length < 2)
             {
-                return BadRequest("El nombre no es valido");
+                return BadRequest("El apellido no es valido");
             }
 
             //var addr = new System.Net.Mail.MailAddress((objeto.Corre).ToString());
@@ -91,5 +91,71 @@ namespace pruebaTecnica.Controllers
                 return StatusCode(StatusCodes.Status200OK, new { mensaje = ex.Message });
             }
         }
+
+        [HttpPut]
+        [Route("Editar")]
+
+        public IActionResult Editar([FromBody] Cliente objeto)
+        {
+            Cliente oCliente = _dbcontext.Clientes.Find(objeto.IdClientes);
+
+            if (oCliente == null)
+            {
+                return BadRequest("Cliente No encontrado");
+            }
+
+            String longitudNombre = (oCliente.Nombres).ToString();
+            String longitudApellido = (oCliente.Apellido).ToString();
+
+            if (longitudNombre.Length < 2)
+            {
+                return BadRequest("El nombre no es valido");
+            }
+
+            if (longitudApellido.Length < 2)
+            {
+                return BadRequest("El apellido no es valido");
+            }
+
+            //var addr = new System.Net.Mail.MailAddress((objeto.Corre).ToString());
+
+            if (Regex.IsMatch((oCliente.Corre).ToString(),
+                @"^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))" + @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$") == false)
+            {
+                return BadRequest("El correo no es valido");
+            }
+
+            DateTime now = DateTime.Today;
+            int edad = DateTime.Today.Year - DateTime.Parse((oCliente.FechaNacimiento).ToString()).Year;
+
+            if (edad < 18)
+            {
+                return BadRequest("El cliente es menor de edad");
+            }
+
+            try
+            {
+                oCliente.TipoIentificacion = objeto.TipoIentificacion is null ? oCliente.TipoIentificacion : objeto.TipoIentificacion;
+                oCliente.NumeroIdentificacion = objeto.NumeroIdentificacion is null ? oCliente.NumeroIdentificacion : objeto.NumeroIdentificacion;
+                oCliente.Nombres = objeto.Nombres is null ? oCliente.Nombres : objeto.Nombres;
+                oCliente.Apellido = objeto.Apellido is null ? oCliente.Apellido : objeto.Apellido;
+                oCliente.Corre = objeto.Corre is null ? oCliente.Corre : objeto.Corre;
+                oCliente.FechaNacimiento = objeto.FechaNacimiento is null ? oCliente.FechaNacimiento : objeto.FechaNacimiento;
+
+                DateTime fechaModificacion = DateTime.Now;
+                oCliente.FechaModificacion = fechaModificacion;
+
+
+                _dbcontext.Clientes.Update(oCliente);
+                _dbcontext.SaveChanges();
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = ex.Message });
+            }
+        }
+
     }
 }
