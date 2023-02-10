51bed61e0ac9fb00147f16ba440b9d7c23d43533
ocastelblack <oscard.castelblanco@gmail.com>
2023-02-10 07:54:30 -0500

    consignacion

--- pruebaTecnica/Controllers/TransaccionesController.cs
+++ pruebaTecnica/Controllers/TransaccionesController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pruebaTecnica.Models;
+using System;
+using System.Text.RegularExpressions;
 
 namespace pruebaTecnica.Controllers
 {
@@ -7,5 +11,43 @@ namespace pruebaTecnica.Controllers
     [ApiController]
     public class TransaccionesController : ControllerBase
     {
+        public readonly PruebaContext _dbcontext;
+
+        public TransaccionesController(PruebaContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        [HttpPost]
+        [Route("CosignacionRetiro")]
+
+        public IActionResult CosignacionRetiro([FromBody] Transaccione objeto)
+        {
+            Producto oProducto = _dbcontext.Productos.Find(objeto.IdProducto);
+            Cliente oCliente = _dbcontext.Clientes.Find(objeto.IdCliente);
+
+            String tipo = (objeto.TipoTransaccion).ToString();
+
+            int saldoAnterior = Int32.Parse((oProducto.Saldo).ToString());
+
+            if (tipo.Equals("Consignación"))
+            {
+                oProducto.Saldo = saldoAnterior - objeto.montoTransaccion;
+                objeto.DescripcionTransaccion = "Cosignacion exitosa para la cuenta correiente " + oProducto.NumeroCuenta + " del cliente " + oCliente.Nombres + oCliente.Apellido;
+            }
+
+            try
+            {
+                _dbcontext.Transacciones.Add(objeto);
+                _dbcontext.Productos.Update(oProducto);
+                _dbcontext.SaveChanges();
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = objeto.DescripcionTransaccion });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = ex.Message });
+            }
+        }
     }
 }
