10ceda63a5ca428351ba10a978c48442490ab282
ocastelblack <oscard.castelblanco@gmail.com>
2023-02-10 01:38:59 -0500

    listar clientes

---
+++ pruebaTecnica/Controllers/ClientesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pruebaTecnica.Models;
+
+namespace pruebaTecnica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        public readonly PruebaContext _dbcontext;
+
+        public ClientesController(PruebaContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+
+        public IActionResult Lista()
+        {
+            List<Cliente> lista = new List<Cliente>();
+
+            try
+            {
+                lista = _dbcontext.Clientes.ToList();  
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", Response = lista });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = ex.Message, Response = lista });
+            }
+        }
+    }
+}
